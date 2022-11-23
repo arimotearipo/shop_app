518b4ae2086d00f799e4b98f89e66e61bb87b92e
Wan Nor Adzahari <wnabwt@yahoo.com>
2022-11-23 16:39:17 +0800

    first commit. added extra navigation tabs

---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop')
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+router.get('/products', shopController.getProduct)
+router.get('/cart', shopController.getCart)
+router.get('/checkout', shopController.getCheckout)
+
+module.exports = router;
