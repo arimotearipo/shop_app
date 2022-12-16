77294eec161ee4569ef12d48f90c863ab72d8162
Wan Nor Adzahari <wnabwt@yahoo.com>
2022-12-16 18:08:41 +0800

    Created deleting product function

--- models/cart.js
+++ models/cart.js
@@ -21,7 +21,7 @@ module.exports = class Cart
 				cart = JSON.parse(fileContent)
 			}
 			// Analyze the product (find if already existing)
-			const existingProductIndex = cart.products.findIndex(prod => prod.id == id)
+			const existingProductIndex = cart.products.findIndex(prod => prod.id === id)
 			const existingProduct = cart.products[existingProductIndex];
 			let updatedProduct;
 			// Add it to an array or just increase the quantity
@@ -38,7 +38,32 @@ module.exports = class Cart
 				cart.products = [...cart.products, updatedProduct]
 			}
 			cart.totalPrice = cart.totalPrice + +productPrice;
-			fs.writeFile(p, JSON.stringify(cart), (err) =>	{
+			fs.writeFile(p, JSON.stringify(cart), (err) =>
+			{
+				console.log(err)
+			})
+		})
+	}
+
+	// Identify item to be deleted using id
+	// Need to also update the price
+	static deleteProduct(id, productPrice)
+	{
+		fs.readFile(p, (err, fileContent) =>
+		{
+			if (err)
+			{
+				return
+			}
+			const updatedCart = {...JSON.parse(fileContent)}
+			const product = updatedCart.products.find(prod => prod.id === id)
+			if (!product)
+				return ;
+			const productQty = product.qty
+			updatedCart.products = updatedCart.products.filter(prod=> prod.id === id)
+			updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty// We need to find out how many numbers of the same item is in the product
+			fs.writeFile(p, JSON.stringify(updatedCart), (err) =>
+			{
 				console.log(err)
 			})
 		})
